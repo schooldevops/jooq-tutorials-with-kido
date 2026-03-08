c013731c36df4c5533f0d2870e58f245c4d8f19f
Baekido <baekido@gmail.com>
2026-03-08 16:04:06 +0900

    fix: resolve Kotlin AuthorEntity getter compilation error

--- chapter-18-jpa-jooq/code-kotlin/src/main/kotlin/com/example/demo/entity/AuthorEntity.kt
+++ chapter-18-jpa-jooq/code-kotlin/src/main/kotlin/com/example/demo/entity/AuthorEntity.kt
@@ -4,13 +4,13 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
-
 import org.springframework.data.domain.Persistable
 
 @Entity
 @Table(name = "author")
 class AuthorEntity(
     @Id
+    @get:JvmName("getIdentifier")
     var id: Int? = null,
 
     @Column(name = "first_name")
@@ -19,6 +19,8 @@ class AuthorEntity(
     @Column(name = "last_name")
     var lastName: String? = null
 ) : Persistable<Int> {
+
     override fun getId(): Int? = id
+
     override fun isNew(): Boolean = true
 }
